353b97e945841aa1cbe02179c180c4632af5a6bc
Anton K <ant.korotun@gmail.com>
2025-08-12 12:20:44 +0300

    moved password encoder to security configuration

--- src/main/java/com/antonk/gymtracker/config/WebConfig.java
+++ src/main/java/com/antonk/gymtracker/config/WebConfig.java
@@ -1,20 +1,12 @@
 package com.antonk.gymtracker.config;
 
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.util.Arrays;
-import java.util.Locale;
 
 @Configuration
 @EnableWebMvc
@@ -32,12 +24,5 @@ public class WebConfig {
                         .allowCredentials(true);
             }
         };
-    }
-
-    @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
-
-        return new BCryptPasswordEncoder(11);
-
     }
 }
